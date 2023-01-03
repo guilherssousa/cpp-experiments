53181cf767906c27024e94377c02a1ec3e70096f
Guilherme Sousa <guilherssousa@gmail.com>
2023-01-03 12:48:32 -0300

    add gen1_checksum.cpp

---
+++ gen1_checksum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdint>
+#include <typeinfo>
+
+#include "read_save_file.h"
+
+int main() {
+    std::ifstream save = readSaveFile("./save.sav");
+
+    uint8_t checksum = 0xff;
+
+    for (int i = 0x2598; i <= 0x3521; i++) {
+        save.seekg(i);
+        char c;
+        save.get(c);
+        checksum -= (uint8_t)c;
+    }
+
+    uint8_t save_checksum;
+
+    save.seekg(0x3523);
+    save.get((char&)save_checksum);
+
+    if(checksum == save_checksum) std::cout << "Checksums match!" << std::endl;
+    else std::cout << "Checksums do not match!" << std::endl;
+
+    std::cout << "Calculated checksum: 0x" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << (int)checksum << std::endl;
+    std::cout << "Save checksum: 0x" << std::setw(2) << std::setfill('0') << std::uppercase << std::hex << (int)save_checksum << std::endl;
+
+    return 0;
+}
